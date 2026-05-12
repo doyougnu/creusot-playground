4f0bfa64e2eeaf95030620f58d66afd0288a6824
Jeffrey Young <jmy6342@gmail.com>
2026-05-12 09:27:25 -0400

    project: start

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+    cruesot.url      = "github:creusot-rs/creusot";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+            rust-analyzer
+            rustfmt
+            creusot
+          ];
+
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-bin.nightly.latest.default
+          ];
+
+          shellHook = ''
+            # alias ls=eza
+            # alias find=fd
+          '';
+        };
+      }
+    );
+}
